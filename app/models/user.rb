a896acee9db8f3f818bc67a39e0aedbb9fec4e56
Timothy Levi Campbell <tlc9406@gmail.com>
2014-02-17 13:52:39 -0500

    Updates User methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,37 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :password, :name
+  attr_reader :first_name, :last_name
 
-  validates :email, :name, :pw_digest, presence: true
+  before_validation :ensure_tokens
+
+  validates :email, :pw_digest, :name, :pwreset_token,
+            :auth_token, :session_token, presence: true
+
+  def self.generate_token
+    SecureRandom.urlsafe_base64(16)
+  end
+
+  def ensure_tokens
+    self.pwreset_token ||= User.generate_token
+    self.auth_token ||= User.generate_token
+    self.session_token ||= User.generate_token
+  end
+
+  def first_name
+    split_name = self.name.split
+    @first_name = split_name.shift
+  end
+
+  def last_name
+    split_name = self.name.split
+    @last_name = split_name.pop
+  end
+
+  def middle_name
+    split_name = self.name.split
+    split_name.shift
+    split_name.pop
+    @middle_name = split_name.join(" ")
+  end
+
 end
