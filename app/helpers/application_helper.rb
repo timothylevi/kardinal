0b944fad3e17fff446cb90eb34908dcd123aa92b
Timothy Levi Campbell <tlc9406@gmail.com>
2014-02-17 15:58:29 -0500

    Adds ApplicationHelper#login, #current_user, #logout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def login(user)
+    session[:token] = user.session_token
+    @current_user = user
+  end
+
+  def current_user
+    return nil unless session[:token]
+    @current_user ||= User.find_by_session_token(session[:token])
+  end
+
+  def logout
+    current_user.reset_session_token!
+    session[:token] = nil
+    # session[:last_visited] = nil
+  end
 end
