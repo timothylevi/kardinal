0829e1c6446bfab9c7b400b01876f73c3ef0bd5f
Timothy Levi Campbell <tlc9406@gmail.com>
2014-02-17 15:55:17 -0500

    Adds ApplicationController#create_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  include SessionsHelper
+  include ApplicationHelper
+
+  def create_user(user)
+    @user = user
+
+    if @user.save
+      login(user)
+      render json: @user
+    else
+      flash.now[:errors] = @user.errors.full_messages
+      render :new
+    end
+  end
 end
