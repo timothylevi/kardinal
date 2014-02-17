11522827257037c97de30760d940add64aa3511b
Timothy Levi Campbell <tlc9406@gmail.com>
2014-02-17 18:57:58 -0500

    Changes error rescuing in SessionsController#create to account for finding user by email...

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,9 @@ class SessionsController < ApplicationController
   end
 
   def create
-    begin
-      @user = User.find_by_email(params[:user][:email])
+    @user = User.find_by_email(params[:user][:email])
 
+    if @user
       if @user.is_password?(params[:user][:password])
         login(@user)
         redirect_to @user
@@ -14,10 +14,12 @@ class SessionsController < ApplicationController
         flash.now[:errors] = ["Username or password is incorrect."]
         render :new
       end
-    rescue
+    else
       @user = User.new(params[:user])
 
       create_user(@user)
+
+      @user.contact_details.create(params[:contact_details])
     end
   end
 
