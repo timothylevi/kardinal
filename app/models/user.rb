0131833b02d387b1f23ac5bd54f33616086f98a3
Timothy Levi Campbell <tlc9406@gmail.com>
2014-02-17 17:00:31 -0500

    Adds comments to User model about specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,48 +2,48 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :name
   attr_reader :first_name, :last_name, :password
 
-  before_validation :ensure_tokens
+  before_validation :ensure_tokens # tested!
 
-  validates :email, uniqueness: true
+  validates :email, uniqueness: true # tested!
   validates :email, :pw_digest, :name, :pwreset_token,
-            :auth_token, :session_token, presence: true
-  validates :password, length: { minimum: 6, allow_nil: true }
+            :auth_token, :session_token, presence: true # tested!
+  validates :password, length: { minimum: 6, allow_nil: true } # tested!
 
-  def self.generate_token
+  def self.generate_token # tested!
     SecureRandom.urlsafe_base64(16)
   end
 
-  def ensure_tokens
+  def ensure_tokens # tested!
     self.pwreset_token ||= User.generate_token
     self.auth_token ||= User.generate_token
     self.session_token ||= User.generate_token
   end
 
-  def reset_session_token!
+  def reset_session_token! # tested!
     self.session_token = User.generate_token
     self.save!
   end
 
-  def password=(secret)
+  def password=(secret) # tested!
     @password = secret
     self.pw_digest = BCrypt::Password.create(secret)
   end
 
-  def is_password?(password)
+  def is_password?(password) # tested!
     BCrypt::Password.new(self.pw_digest).is_password?(password)
   end
 
-  def first_name
+  def first_name # tested!
     split_name = self.name.split
     @first_name = split_name.shift
   end
 
-  def last_name
+  def last_name # tested!
     split_name = self.name.split
     @last_name = split_name.pop
   end
 
-  def middle_name
+  def middle_name # tested!
     split_name = self.name.split
     split_name.shift
     split_name.pop
