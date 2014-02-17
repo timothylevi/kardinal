e26f155421e0a3fb538c219140eb3f809de10d20
Timothy Levi Campbell <tlc9406@gmail.com>
2014-02-17 15:57:51 -0500

    Adds a begin/rescue/end to rescue RecordInvalid errors

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,26 +7,17 @@ class SessionsController < ApplicationController
     begin
       @user = User.find_by_email(params[:user][:email])
 
-    if @user
       if @user.is_password?(params[:user][:password])
-        # build login functionality
         login(@user)
         render json: @user
       else
         flash.now[:errors] = ["Username or password is incorrect."]
         render :new
       end
-    else
-      # this needs to be it's own function
+    rescue
       @user = User.new(params[:user])
 
-      if @user.save
-        login(@user)
-        render json: @user
-      else
-        flash.now[:errors] = @user.errors.full_messages
-        render :new
-      end
+      create_user(@user)
     end
   end
 
