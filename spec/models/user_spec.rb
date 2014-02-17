4e4231b3f0a6faa2f32739007b5e8b305710f070
Timothy Levi Campbell <tlc9406@gmail.com>
2014-02-17 13:52:04 -0500

    Changes User spec to test User#ensure_tokens

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,6 +13,50 @@ describe User do
     expect(FactoryGirl.build(:user, pw_digest: nil)).not_to be_valid
   end
 
-  it "splits name into first and last"
-  it "validates presence of zip"
+  it "validates presence of password-reset token" do
+    user = FactoryGirl.build(:user, pwreset_token: nil)
+    expect(user).to be_valid
+  end
+
+  it "validates presence of authorization token" do
+    user = FactoryGirl.build(:user, auth_token: nil)
+    expect(user).to be_valid
+  end
+
+  it "validates presence of session token" do
+    user = FactoryGirl.build(:user, session_token: nil)
+    expect(user).to be_valid
+  end
+
+  it "splits name into first and last" do
+    user = FactoryGirl.build(:user, name: "Timothy Campbell")
+    expect(user.first_name).to eq("Timothy")
+    expect(user.last_name).to eq("Campbell")
+  end
+
+  it "splits name into first, middle, and last" do
+    user = FactoryGirl.build(:user, name: "Timothy Levi Campbell")
+    expect(user.first_name).to eq("Timothy")
+    expect(user.middle_name).to eq("Levi")
+    expect(user.last_name).to eq("Campbell")
+  end
+
+  it "can split name into multiple middle names" do
+    user = FactoryGirl.build(:user, name: "Timothy Levi Warner Campbell")
+    expect(user.middle_name).to eq("Levi Warner")
+  end
+
+  it "creates tokens before saving" do
+    user = FactoryGirl.create(:user)
+    expect(user.pwreset_token).not_to be_nil
+    expect(user.auth_token).not_to be_nil
+    expect(user.session_token).not_to be_nil
+  end
+
+  it "class generates a random token" do
+    token1 = User.generate_token
+    token2 = User.generate_token
+    expect(token1).not_to eq(token2)
+  end
+
 end
