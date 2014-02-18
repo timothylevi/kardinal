2e3b844d747117bdaaa24bb7399da1446e9fb894
Timothy Levi Campbell <tlc9406@gmail.com>
2014-02-18 15:08:52 -0500

    Creates Petition show view and contrller action

--- app/controllers/petitions_controller.rb
+++ app/controllers/petitions_controller.rb
@@ -2,4 +2,9 @@ class PetitionsController < ApplicationController
   def index
     @petitions = Petition.all
   end
+
+  def show
+    @petition = Petition.find(params[:id])
+    @creator = @petition.creator
+  end
 end
