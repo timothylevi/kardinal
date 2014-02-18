1db62ff549f26c2d3535ebca9fd9c9521e8b169c
Timothy Levi Campbell <tlc9406@gmail.com>
2014-02-18 18:54:59 -0500

    Creates attr_accessible, validations, and associations for PetitionSignature model

--- app/models/petition_signatures.rb
+++ app/models/petition_signatures.rb
@@ -10,5 +10,10 @@
 #
 
 class PetitionSignatures < ActiveRecord::Base
-  # attr_accessible :title, :body
+  attr_accessible :user_id, :petition_id
+
+  validates :user_id, :petition_id, presence: true
+
+  belongs_to :user
+  belongs_to :petition
 end
