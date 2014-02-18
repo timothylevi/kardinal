35805f69e8957d869cd990dca2b777c606298f7c
Timothy Levi Campbell <tlc9406@gmail.com>
2014-02-18 10:50:51 -0500

    Changed back to User has_many ContactDetails to make my life a little easier

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   include ApplicationHelper
 
   def create_user(user, contact)
-    user.save && user.contact_deatil.create(contact)
+    user.save && user.contact_details.create(contact)
     return user
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,4 +12,20 @@ class UsersController < ApplicationController
     @user = current_user
     @contact_detail = @user.contact_details.first
   end
+
+  def update
+    @user = current_user
+    @contact_detail = @user.contact_details.first
+
+    if @user.update_attributes(params[:user]) &&
+       @contact_detail.update_attributes(params[:contact_details])
+      redirect_to @user
+    else
+      flash.now[:errors] ||= []
+      flash.now[:errors] << @user.errors.full_messages if @user.errors
+      flash.now[:errors] << @contact_detail.errors.full_messages if
+        @contact_detail.errors
+      render :edit
+    end
+  end
 end
