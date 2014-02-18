997b847837a618e99babc037383c4079f3de13d2
Timothy Levi Campbell <tlc9406@gmail.com>
2014-02-18 11:15:17 -0500

    Adds @contact_detail to both User#show and User#me

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,12 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @contact_detail = @user.contact_details.first
   end
 
   def me
     @user = current_user
-    render :show
+    @contact_detail = @user.contact_details.first
   end
 
   def edit
