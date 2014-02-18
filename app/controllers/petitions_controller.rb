a81bdbf9c4e84440d8ec1b0ec0363940a61fdd9d
Timothy Levi Campbell <tlc9406@gmail.com>
2014-02-18 15:54:34 -0500

    Creates PetitionsController#new, #create, and Petition new view

--- app/controllers/petitions_controller.rb
+++ app/controllers/petitions_controller.rb
@@ -7,4 +7,22 @@ class PetitionsController < ApplicationController
     @petition = Petition.find(params[:id])
     @creator = @petition.creator
   end
+
+  def new
+    @petition = Petition.new
+  end
+
+  def create
+    current_user.petitions.build(params[:petition])
+
+    if current_user.save
+      flash[:notices] = "Petition saved!"
+      redirect_to petition_url(current_user.petitions.last)
+    else
+      @petition = Petition.new(params[:petition])
+      @petition.valid?
+
+      render :new
+    end
+  end
 end
