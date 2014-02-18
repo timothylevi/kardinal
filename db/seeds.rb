adb23a5a0b80372dd9f2b3fa6229f8ec33fcf2ad
Timothy Levi Campbell <tlc9406@gmail.com>
2014-02-18 14:20:57 -0500

    Legit seed file! I feel like I can work with something now...

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+100.times do |i|
+  User.create(email: Faker::Internet.safe_email,
+              password: Faker::Internet.password,
+              name: Faker::Name.name)
+end
+
+User.all.each do |user|
+  user.contact_details.create(
+    street_address: Faker::Address.street_address,
+    city: Faker::Address.city,
+    state: Faker::Address.state_abbr,
+    zip: Faker::Address.zip[0..4],
+    phone: Faker::PhoneNumber.phone_number,
+    country: "United States",
+    description: Faker::Lorem.paragraph(6),
+    website: Faker::Internet.url)
+end
+
+i = User.first.id
+
+while i < User.last.id do
+  if (i % 3 == 0)
+    user = User.find(i)
+    user.petitions.create(
+      title: Faker::Company.catch_phrase,
+      body: Faker::Lorem.paragraph(3),
+      background: Faker::Lorem.paragraph(6))
+  end
+
+  i += 1
+end
