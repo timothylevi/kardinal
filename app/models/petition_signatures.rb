d90dcb52ba445e3c626de36252bcf13157042bf8
Timothy Levi Campbell <tlc9406@gmail.com>
2014-02-18 18:47:08 -0500

    Annotates PetitionSignature model, factory, and spec

--- app/models/petition_signatures.rb
+++ app/models/petition_signatures.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: petition_signatures
+#
+#  id          :integer          not null, primary key
+#  user_id     :integer          not null
+#  petition_id :integer          not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class PetitionSignatures < ActiveRecord::Base
   # attr_accessible :title, :body
 end
--- spec/models/petition_signatures_spec.rb
+++ spec/models/petition_signatures_spec.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: petition_signatures
+#
+#  id          :integer          not null, primary key
+#  user_id     :integer          not null
+#  petition_id :integer          not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 require 'spec_helper'
 
 describe PetitionSignatures do
