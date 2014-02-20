b1c3ec33518d6f430ffb7eac8c08d1928a0b5e8b
Timothy Levi Campbell <tlc9406@gmail.com>
2014-02-20 03:27:10 -0500

    Changes blank title attribute to be two dashes

--- app/models/contact_detail.rb
+++ app/models/contact_detail.rb
@@ -34,9 +34,9 @@ class ContactDetail < ActiveRecord::Base
   belongs_to :contactable, polymorphic: true
 
   def self.states
-    %w(n/a AL AK AZ AR CA CO CT DE DC FL GA HI ID IL IN IA KS KY
-           LA ME MD MA MI MN MS MO MT NE NV NH NJ NM NY NC ND OH
-           OK OR PA RI SC SD TN TX UT VT VA WA WV WI WY PR AS GU
-           MP VI UM FM PW)
+    %w(-- AL AK AZ AR CA CO CT DE DC FL GA HI ID IL IN IA KS KY
+          LA ME MD MA MI MN MS MO MT NE NV NH NJ NM NY NC ND OH
+          OK OR PA RI SC SD TN TX UT VT VA WA WV WI WY PR AS GU
+          MP VI UM FM PW)
   end
 end
