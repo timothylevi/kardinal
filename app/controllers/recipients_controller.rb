3bf22a42688cb6d41c1d0bf660981faa0e1b3de2
Timothy Levi Campbell <tlc9406@gmail.com>
2014-02-20 03:23:27 -0500

    Creates RecipientsController#create

--- app/controllers/recipients_controller.rb
+++ app/controllers/recipients_controller.rb
@@ -1,5 +1,23 @@
 class RecipientsController < ApplicationController
   def new
     @recipient = Recipient.new
+    @contact_detail = ContactDetail.new
+  end
+
+  def create
+    @recipient = Recipient.new(params[:recipient])
+    @contact_detail = ContactDetail.new(params[:contact_detail])
+
+    if @recipient.save &&
+      @recipient.contact_details.create(params[:contact_detail])
+      flash[:notices] = "#{@recipient.first_name} #{@recipient.last_name} was successfully saved!"
+
+      redirect_to new_petition_url
+    else
+      @contact_detail = @recipient.contact_details.new(params[:contact_detail])
+      @contact_detail.valid?
+
+      render :new
+    end
   end
 end
