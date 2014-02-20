1429e22ecd85f681ceb46e5f8b9fb21cd2945361
Timothy Levi Campbell <tlc9406@gmail.com>
2014-02-20 01:47:59 -0500

    Creates PetitionRecipient::find_single to mask find_by_recipient_id_and_petition_id

--- app/models/petition_recipient.rb
+++ app/models/petition_recipient.rb
@@ -16,4 +16,9 @@ class PetitionRecipient < ActiveRecord::Base
 
   belongs_to :petition
   belongs_to :recipient
+
+  def self.find_single(recipient_id, petition_id)
+    PetitionRecipient.find_by_recipient_id_and_petition_id(recipient_id, petition_id)
+  end
+
 end
