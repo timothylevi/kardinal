251b66cdb0230b591ae5bcf05397509b1ff646e1
Timothy Levi Campbell <tlc9406@gmail.com>
2014-02-20 11:03:12 -0500

    Adds 'paperclip' config in production.rb and development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,13 @@ Cardinal::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  config.paperclip_defaults = {
+        :storage => :s3,
+        :s3_credentials => {
+          :bucket => ENV["S3_BUCKET"],
+          :access_key_id => ENV["S3_KEY"],
+          :secret_access_key => ENV["S3_SECRET"]
+        }
+      }
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,13 @@ Cardinal::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  config.paperclip_defaults = {
+        :storage => :s3,
+        :s3_credentials => {
+          :bucket => ENV["S3_BUCKET"],
+          :access_key_id => ENV["S3_KEY"],
+          :secret_access_key => ENV["S3_SECRET"]
+        }
+      }
 end
