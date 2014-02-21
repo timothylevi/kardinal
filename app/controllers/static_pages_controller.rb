da26b07dce2e23122e5d91884701d5a223cf7ccb
App Academy Student <student@appacademy.io>
2014-02-21 15:11:41 -0500

    Adds StaticPagesController and #root view

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,2 +1,11 @@
 class StaticPagesController < ApplicationController
+  def root
+    @user = User.new
+    @petitions = Petition.includes(:creator, :petition_signatures)
+            .order("created_at DESC")
+            .where(is_victory: false)
+            .limit(10)
+    @victories = Victory.includes(petition: [:creator, :petition_signatures])
+            .limit(5)
+  end
 end
