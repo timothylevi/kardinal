9e1d0b9ad5b0c569cbce76b8b61ce01c5eca83ce
App Academy Student <student@appacademy.io>
2014-02-21 12:46:54 -0500

    Requests specific information from Facebook

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FB_KEY'], ENV['FB_SECRET'],
-    :image_size => 'normal'
+    :image_size => 'large', :info_fields => "email, name, website, location"
 end
