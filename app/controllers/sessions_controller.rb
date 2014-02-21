892bbc2628641818ca1a7502f81fc085909c415b
App Academy Student <student@appacademy.io>
2014-02-21 12:42:59 -0500

    Facilitates omniauth in the SessionsController

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,27 +6,44 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by_email(params[:user][:email])
+    if request.env['omniauth.auth']
+      fb_data = request.env['omniauth.auth']
+      @user = User.find_by_uid(fb_data[:uid])
 
-    if @user
-      if @user.is_password?(params[:user][:password])
+      if @user
         login(@user)
         flash[:notices] = "Welcome back, #{@user.first_name}!"
-        redirect_to petitions_url
+        redirect_to me_url
       else
-        flash.now[:errors] = "Your username or password is incorrect."
-        render :new
-      end
-    else
-      @user = User.new(params[:user])
-      contact = params[:contact_details]
-
-      if create_user(@user, contact).valid?
+        @user = User.create_from_fb(fb_data)
         login(@user)
         flash[:notices] = "Welcome, #{@user.first_name}!"
-        redirect_to me_edit_url
+        redirect_to me_url
+      end
+
+    else
+      @user = User.find_by_email(params[:user][:email])
+
+      if @user
+        if @user.is_password?(params[:user][:password])
+          login(@user)
+          flash[:notices] = "Welcome back, #{@user.first_name}!"
+          redirect_to root_url
+        else
+          flash.now[:errors] = "Your username or password is incorrect."
+          render :new
+        end
       else
-        render :new
+        @user = User.new(params[:user])
+        contact = params[:contact_details]
+
+        if create_user(@user, contact).valid?
+          login(@user)
+          flash[:notices] = "Welcome, #{@user.first_name}!"
+          redirect_to me_edit_url
+        else
+          render :new
+        end
       end
     end
   end
