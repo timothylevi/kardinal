dc3984b5314444ad3e2bde9a74fbc372f5d07a77
Timothy Levi Campbell <tlc9406@gmail.com>
2014-02-24 19:45:29 -0500

    Creates a new secret token and passes it to figaro

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Cardinal::Application.config.secret_token = 'bc2b18e76dc57fae8dfd08a8d107283220f5e9677722927d5270dcc9fde011bd8c1cc67fbdb81fbaa12d947a4c3f0e486cb04fc99f6a8767ab3bc315bcb34ca8'
+Cardinal::Application.config.secret_token = ENV["SECRET_TOKEN"]
