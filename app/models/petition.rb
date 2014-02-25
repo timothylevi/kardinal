b10a19682b48f02db50cf8dca566e7e8c6c69627
Timothy Levi Campbell <tlc9406@gmail.com>
2014-02-25 15:05:54 -0500

    Annotates Petition files to include the paperclip columns.

--- app/models/petition.rb
+++ app/models/petition.rb
@@ -2,16 +2,20 @@
 #
 # Table name: petitions
 #
-#  id         :integer          not null, primary key
-#  creator_id :integer          not null
-#  title      :string(255)      not null
-#  body       :text             not null
-#  background :text             not null
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#  approved   :string(255)      default("Pending")
-#  goal       :integer
-#  is_victory :boolean          default(FALSE)
+#  id                 :integer          not null, primary key
+#  creator_id         :integer          not null
+#  title              :string(255)      not null
+#  body               :text             not null
+#  background         :text             not null
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  approved           :string(255)      default("Pending")
+#  goal               :integer
+#  is_victory         :boolean          default(FALSE)
+#  image_file_name    :string(255)
+#  image_content_type :string(255)
+#  image_file_size    :integer
+#  image_updated_at   :datetime
 #
 
 class Petition < ActiveRecord::Base
