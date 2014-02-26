a4b6a0ec6ee7c6a183122d02348e02a3dbb174fc
Timothy Levi Campbell <tlc9406@gmail.com>
2014-02-26 11:14:48 -0500

    Creates polymorphic commentable association for comment as well as belongs to user association

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,10 @@
 class Comment < ActiveRecord::Base
-  # attr_accessible :title, :body
+  attr_accessible :user_id, :commentable_id, :commentable_type, :body
+
+  belongs_to :commentable, polymorphic: true
+  belongs_to :user,
+    class_name: "User",
+    foreign_key: :user_id,
+    primary_key: :id
+
 end
