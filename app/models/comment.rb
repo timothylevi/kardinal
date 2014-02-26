d80584ef195b26f593062757682c84c1eee356c3
Timothy Levi Campbell <tlc9406@gmail.com>
2014-02-26 11:26:02 -0500

    Annotates Comment files

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id               :integer          not null, primary key
+#  user_id          :integer
+#  commentable_id   :integer
+#  commentable_type :string(255)
+#  body             :string(255)
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
+
 class Comment < ActiveRecord::Base
   attr_accessible :user_id, :commentable_id, :commentable_type, :body
 
--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id               :integer          not null, primary key
+#  user_id          :integer
+#  commentable_id   :integer
+#  commentable_type :string(255)
+#  body             :string(255)
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
+
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
