e055f0d58745df5fd38d21c2aa2267506a5c4781
Timothy Campbell <tlc9406@gmail.com>
2014-02-27 21:01:45 -0500

    Updates VictoriesController#index to handle pagination

--- app/controllers/victories_controller.rb
+++ app/controllers/victories_controller.rb
@@ -1,6 +1,8 @@
 class VictoriesController < ApplicationController
   def index
-    @victories = Victory.includes(petition: [:creator, :petition_signatures]).limit(10)
+    @victories = Victory.includes(petition: [:creator, :petition_signatures])
+                        .order("created_at DESC")
+                        .page(params[:page])
     @causes = Cause.order(:name)
   end
 
