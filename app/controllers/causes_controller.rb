00c102a31cdcb6148edcd9cce520bc3f7bfc954a
Timothy Campbell <tlc9406@gmail.com>
2014-02-28 00:50:15 -0500

    Passes victories and causes to the causes/show view

--- app/controllers/causes_controller.rb
+++ app/controllers/causes_controller.rb
@@ -1,7 +1,9 @@
 class CausesController < ApplicationController
   def show
     @cause = Cause.find_by_name(params[:id]) || Cause.find(params[:id])
-
+    @causes = Cause.all
     @petitions = @cause.petitions
+    @victories = Victory.includes(petition: [:creator, :petition_signatures])
+            .limit(5)
   end
 end
