06ed97fa93db1e330c3912d855dabb413e7fa6e5
Yarossia <gtibmx@yandex.ru>
2015-12-12 19:14:54 +0200

    login form

---
+++ app/assets/javascripts/script_login.js
@@ -0,0 +1,20 @@
+var regcheck;
+$(window).load(function() {
+
+    $("#registration").click(function(){
+        regcheck=1;
+        $("#send").val("Зареєструватися")
+
+    })
+    $("#enter").click(function(){
+        regcheck=0;
+        $("#send").val("Увійти")
+    })
+    $("#send").click(function(){
+        if (regcheck){
+        //$(".button_facebook_div")
+        $(".main").text("���������")//.css('backgroundColor',"blue");;
+        }
+    })
+
+})
