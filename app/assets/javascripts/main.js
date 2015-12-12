e63c2c47c2f8ee4ea3538edf81512713262f1c09
Yarossia <gtibmx@yandex.ru>
2015-12-13 00:30:26 +0200

    general page

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,127 @@
+/*
+	Alpha by HTML5 UP
+	html5up.net | @n33co
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+$(document).ready(function() {
+
+	var owl = $("#owl-demo");
+
+	owl.owlCarousel({
+		items : 1, //10 items above 1000px browser width
+		itemsDesktop : [1000,1], //5 items between 1000px and 901px
+		itemsDesktopSmall : [900,1], // betweem 900px and 601px
+		itemsTablet: [600,1], //2 items between 600 and 0
+		itemsMobile : false // itemsMobile disabled - inherit from itemsTablet option
+	});
+	owl.trigger('owl.play',4000); //owl.play event accept autoPlay speed as second parameter
+	// Custom Navigation Events
+	//$(".next").click(function(){
+	//	owl.trigger('owl.next');
+	//})
+	//$(".prev").click(function(){
+	//	owl.trigger('owl.prev');
+	//})
+	//$(".play").click(function(){
+	//	owl.trigger('owl.play',1000); //owl.play event accept autoPlay speed as second parameter
+	//})
+	//$(".stop").click(function(){
+	//	owl.trigger('owl.stop');
+	//})
+
+});
+
+(function($) {
+
+	skel.breakpoints({
+		wide: '(max-width: 1680px)',
+		normal: '(max-width: 1280px)',
+		narrow: '(max-width: 980px)',
+		narrower: '(max-width: 840px)',
+		mobile: '(max-width: 736px)',
+		mobilep: '(max-width: 480px)'
+	});
+
+	$(function() {
+
+		var	$window = $(window),
+			$body = $('body'),
+			$header = $('#header'),
+			$banner = $('#banner');
+
+		// Fix: Placeholder polyfill.
+			$('form').placeholder();
+
+		// Prioritize "important" elements on narrower.
+			skel.on('+narrower -narrower', function() {
+				$.prioritize(
+					'.important\\28 narrower\\29',
+					skel.breakpoint('narrower').active
+				);
+			});
+
+		// Dropdowns.
+			$('#nav > ul').dropotron({
+				alignment: 'right'
+			});
+
+		// Off-Canvas Navigation.
+
+			// Navigation Button.
+				$(
+					'<div id="navButton">' +
+						'<a href="#navPanel" class="toggle"></a>' +
+					'</div>'
+				)
+					.appendTo($body);
+
+			// Navigation Panel.
+				$(
+					'<div id="navPanel">' +
+						'<nav>' +
+							$('#nav').navList() +
+						'</nav>' +
+					'</div>'
+				)
+					.appendTo($body)
+					.panel({
+						delay: 500,
+						hideOnClick: true,
+						hideOnSwipe: true,
+						resetScroll: true,
+						resetForms: true,
+						side: 'left',
+						target: $body,
+						visibleClass: 'navPanel-visible'
+					});
+
+			// Fix: Remove navPanel transitions on WP<10 (poor/buggy performance).
+				if (skel.vars.os == 'wp' && skel.vars.osVersion < 10)
+					$('#navButton, #navPanel, #page-wrapper')
+						.css('transition', 'none');
+
+		// Header.
+		// If the header is using "alt" styling and #banner is present, use scrollwatch
+		// to revert it back to normal styling once the user scrolls past the banner.
+		// Note: This is disabled on mobile devices.
+			if (!skel.vars.mobile
+			&&	$header.hasClass('alt')
+			&&	$banner.length > 0) {
+
+				$window.on('load', function() {
+
+					$banner.scrollwatch({
+						delay:		0,
+						range:		0.5,
+						anchor:		'top',
+						on:			function() { $header.addClass('alt reveal'); },
+						off:		function() { $header.removeClass('alt'); }
+					});
+
+				});
+
+			}
+
+	});
+
+})(jQuery);
