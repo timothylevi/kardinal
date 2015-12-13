dba4a5bebf9c661018074caa46501abda33d8ce4
Yarossia <gtibmx@yandex.ru>
2015-12-13 06:37:17 +0200

    add promo circles

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -3,7 +3,10 @@
 	html5up.net | @n33co
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
+
 $(document).ready(function() {
+
+
 
 	var owl = $("#owl-demo");
 
@@ -32,6 +35,9 @@ $(document).ready(function() {
 });
 
 (function($) {
+	$( "#closeBtn" ).click(function() {
+		$( ".box" ).hide();
+	});
 
 	skel.breakpoints({
 		wide: '(max-width: 1680px)',
